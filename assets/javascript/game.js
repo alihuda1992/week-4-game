81edb0b1466489d291904205e5e03a825d21589c
Ali Huda <alihuda1992@gmail.com>
2018-03-03 17:40:06 -0500

    updating

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var winning;
+var losses;
+var number;
+
+wins= 0;
+losses = 0;
+number = 0;
+
+        document.getElementById("W").innerHTML=wins;
+        document.getElementById("L").innerHTML=losses;
+        document.getElementById("N").innerHTML=number;       
+   
+        
