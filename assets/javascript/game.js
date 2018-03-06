2a92e8902c0086e1abbc2d63a820c172cb893a8f
CL\ahuda <ahuda@cardlytics.com>
2018-03-05 23:27:24 -0500

    updating style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
 var winning = 0;
 var losses =0;
-var number =0;
+var number = Math.floor(Math.random() * 100) + 1;
+var userInput =0;
+var button1 = Math.floor(Math.random() * 10) + 1;
 
 window.onload = function() {
 document.getElementById("W").innerHTML=winning;
 document.getElementById("L").innerHTML=losses;
 document.getElementById("N").innerHTML= number;
-}
+document.getElementById("y").innerHTML= userInput;
 
+}
