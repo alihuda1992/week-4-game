abc1b0148e2ee9dea37375bf80ffd98355a16f17
CL\ahuda <ahuda@cardlytics.com>
2018-03-07 16:42:45 -0500

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,94 @@
+//Globally defined variables 
 var winning = 0;
-var losses =0;
+var losses = 0;
 var number = Math.floor(Math.random() * 100) + 1;
-var userInput =0;
+var userInput = 0;
 var button1 = Math.floor(Math.random() * 10) + 1;
+var button2 = Math.floor(Math.random() * 10) + 1;
+var button3 = Math.floor(Math.random() * 10) + 1;
+var button4 = Math.floor(Math.random() * 10) + 1;
 
-window.onload = function() {
-document.getElementById("W").innerHTML=winning;
-document.getElementById("L").innerHTML=losses;
-document.getElementById("N").innerHTML= number;
-document.getElementById("y").innerHTML= userInput;
+//On load of document assign these html elements the following values from the above correlating javascript variables
+$(document).ready(function () {
+    $("#win").text(winning);
+    $("#loss").text(losses);
+    $("#number").text(number);
+    $("#your").text(userInput);
 
-}
+
+
+    $("#button1").click(function () {
+        var valueholder = button1;
+        userInput = userInput + valueholder;
+        $("#your").text(userInput);
+        console.log(valueholder);
+
+        if (userInput > number) {
+            var valueholder = 1;
+            losses = losses + valueholder;
+            $("#loss").text(losses);
+        } else if (userInput === number) {
+            var valueholder = 1;
+            winning = winning + valueholder;
+            $("#win").text(winning);
+        }
+    });
+
+    $("#button2").click(function () {
+        var valueholder = button2;
+        userInput = userInput + valueholder;
+        $("#your").text(userInput);
+        console.log(valueholder);
+
+        if (userInput > number) {
+            var valueholder = 1;
+            losses = losses + valueholder;
+            $("#loss").text(losses);
+        } else if (userInput === number) {
+            var valueholder = 1;
+            winning = winning + valueholder;
+            $("#win").text(winning);
+        }
+    });
+
+    $("#button3").click(function () {
+        var valueholder = button3;
+        userInput = userInput + valueholder;
+        $("#your").text(userInput);
+        console.log(valueholder);
+
+        if (userInput > number) {
+            var valueholder = 1;
+            losses = losses + valueholder;
+            $("#loss").text(losses);
+        } else if (userInput === number) {
+            var valueholder = 1;
+            winning = winning + valueholder;
+            $("#win").text(winning);
+        }
+    });
+
+    $("#button4").click(function () {
+        var valueholder = button4;
+        userInput = userInput + valueholder;
+        $("#your").text(userInput);
+        console.log(valueholder);
+
+        if (userInput > number) {
+            var valueholder = 1;
+            losses = losses + valueholder;
+            $("#loss").text(losses);
+        } else if (userInput === number) {
+            var valueholder = 1;
+            winning = winning + valueholder;
+            $("#win").text(winning);
+        }
+    });
+});
+
+
+
+
+
+
+
